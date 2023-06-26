4fa069f290f61faba969ea7ec65db598a9b5372e
Henrik Larsen <henrik7264@outlook.com>
2023-06-26 22:02:08 +0200

    Updated copyrights notice.

--- py_actors/example_statemachine/publish_actor.py
+++ py_actors/example_statemachine/publish_actor.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2023, Henrik Larsen
+# https://github.com/henrik7264/PY_Actors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
